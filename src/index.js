e80048402738deaef865f258d1f7a01a27b01bb9
Kool Havis <havisk@gmail.com>
2019-06-10 14:25:08 -0400

    created task model and set up endpoints to create task, get users and sing user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("./db/mongoose");
+
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,7 +15,46 @@ app.post("/users", (req, res) => {
   user
     .save()
     .then(() => {
+      res.status(201).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+app.get("/users", (req, res) => {
+  User.find({})
+    .then(users => {
+      res.send(users);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send();
+      }
       res.send(user);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+
+  // console.log(req.params);
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
     })
     .catch(e => {
       res.status(400).send(e);
