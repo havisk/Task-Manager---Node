a8406ab7cafb20db1e6d5f8a279997d6172491f3
Kool Havis <havisk@gmail.com>
2019-06-10 14:55:30 -0400

    created end point to read all and single task

--- src/index.js
+++ src/index.js
@@ -44,8 +44,6 @@ app.get("/users/:id", (req, res) => {
     .catch(e => {
       res.status(500).send();
     });
-
-  // console.log(req.params);
 });
 
 app.post("/tasks", (req, res) => {
@@ -58,6 +56,31 @@ app.post("/tasks", (req, res) => {
     })
     .catch(e => {
       res.status(400).send(e);
+    });
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find()
+    .then(task => {
+      res.send(task);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then(task => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch(e => {
+      res.status(500).send();
     });
 });
 
