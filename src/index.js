a5401f42921333224883e3b1ae89482a876c77de
Kool Havis <havisk@gmail.com>
2019-06-12 13:35:14 -0400

    hashed passwords

--- src/index.js
+++ src/index.js
@@ -3,157 +3,31 @@ require("./db/mongoose");
 
 const User = require("./models/user");
 const Task = require("./models/task");
+const userRouter = require("./routers/user-router");
+const taskRouter = require("./routers/task-router");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send();
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.send(500).send();
-  }
-});
-
-app.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "age"];
-  const isValidOperation = updates.every(update =>
-    allowedUpdates.includes(update)
-  );
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid Updates!" });
-  }
-
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(400).send();
-  }
-});
-
-app.delete("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findByIdAndDelete(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send();
-  }
-});
-
-app.get("/tasks", async (req, res) => {
-  try {
-    const task = await Task.find({});
-    res.send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.patch("/tasks/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedToUpdate = ["description", "completed"];
-  const isValidUpdate = updates.every(update =>
-    allowedToUpdate.includes(update)
-  );
-
-  if (!isValidUpdate) {
-    return res.status(400).send({ error: "Invalid Updates!" });
-  }
-
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-    if (!task) {
-      res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(400).send();
-  }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findByIdAndDelete(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
+
+const bcrypt = require("bcrypt");
+
+const myFunc = async () => {
+  const password = "red12345!";
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare("red12345!", hashedPassword);
+
+  console.log(isMatch);
+};
+myFunc();
